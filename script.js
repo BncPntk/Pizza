868c2c8f72b394a7c92dce3c7663b10325da69e4
Bence Pentek <pntkbence@gmail.com>
2022-04-12 14:23:41 +0200

    generating all items

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/BncPntk/Pizza/main/images/'
+const container2 = document.querySelector('#container2');
+const pizzaScroll = document.querySelector('.pizzaScroll');
+const baseURL = 'https://raw.githubusercontent.com/BncPntk/Pizza/main/images/menu/'
+// BTNS
 
 const menu = [
     {},
@@ -72,30 +75,30 @@ const menu = [
         (allergének: glutén, földimogyoró)`,
         price: '2890',
     },
-    // {
-    //     id: 11,
-    //     name: '',
-    //     desc: ``,
-    //     price: '',
-    // },
-    // {
-    //     id: 12,
-    //     name: '',
-    //     desc: ``,
-    //     price: '',
-    // },
-    // {
-    //     id: 13,
-    //     name: '',
-    //     desc: ``,
-    //     price: '',
-    // },
-    // {
-    //     id: 14,
-    //     name: '',
-    //     desc: ``,
-    //     price: '',
-    // },
+    {
+        id: 11,
+        name: 'Basic pita',
+        desc: `Fetakrém, tzatziki, olívabogyó, paradicsom, hagyma, csirkecomb`,
+        price: '1790',
+    },
+    {
+        id: 12,
+        name: 'Extra pita',
+        desc: `Eredeti görög feta sajt, eredeti görög joghurt, Cserpes füstölt sajt, répasaláta, tzatziki, olívabogyó, paradicsom, hagyma, extra mennyiségű csirkecomb`,
+        price: '2090',
+    },
+    {
+        id: 13,
+        name: 'Jalapeno pita',
+        desc: `eredeti görög feta sajt, eredeti görög joghurt, olívabogyó, paradicsom, hagyma, jalapeno paprika, csirkecomb`,
+        price: '2090',
+    },
+    {
+        id: 14,
+        name: 'Gyros tál',
+        desc: `eredeti görög feta sajt, eredeti görög joghurt, olívabogyó, paradicsom, hagyma, csirkecomb, fűszeres hasábburgonya`,
+        price: '2490',
+    },
     // {
     //     id: 15,
     //     name: '',
@@ -116,58 +119,115 @@ const menu = [
     // },
 
 
-
 ]
+
+
+
+
+
+
 // ÁR FORMÁZÁSA
-for (let i = 1; i < menu.length; i++) {
+for (let i = 1; i < 15; i++) {
     let temp = menu[i].price.slice(1);
     let newPrice = `HUF ${menu[i].price[0]},${temp}`
     // console.log(newPrice);
     menu[i].price = newPrice;
 }
+// SCROLL TO 
+
 // ÁR FORMÁZÁSA
 
+// INSERT PIZZA
+const insertItems = function (imageFrom, imageTo, destination) {
+    for (let i = imageFrom; i <= imageTo; i++) {
+        const colDiv = document.createElement('div');
+        colDiv.classList.add('col-10', 'offset-1', 'offset-md-0', 'col-md-6', 'col-lg-4', 'my-4');
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        const newImg = document.createElement('img');
+        newImg.classList.add('rounded-top');
+        newImg.src = `${baseURL}${i}.jpg`;
+        const cardBodyDiv = document.createElement('div');
+        cardBodyDiv.classList.add('card-body');
+        const h5 = document.createElement('h5');
+        h5.classList.add('card-title', 'my-3', 'text-uppercase');
+        h5.innerHTML = `${menu[i].name}`;
+        const p = document.createElement('p');
+        p.classList.add('card-text', 'p-height', 'descFont');
+        p.innerHTML = `${menu[i].desc}`;
+        const inputDiv = document.createElement('div');
+        inputDiv.classList.add('form-outline', 'd-flex', 'justify-content-center', 'my-1', 'mb-3');
+        const input = document.createElement('input');
+        input.classList.add('form-control', 'text-center');
+        input.setAttribute('id', 'typeNumber');
+        input.setAttribute('type', 'number');
+        input.setAttribute('style', 'width: 5rem');
+        input.setAttribute('min', 1);
+        input.setAttribute('max', 5);
+        const pPrice = document.createElement('p');
+        pPrice.classList.add('text-center', 'text-primary', 'fw-bold', 'priceText');
+        pPrice.innerHTML = `${menu[i].price}`;
+        const a = document.createElement('a');
+        a.classList.add('btn', 'btn-outline-warning', 'kosarBtn');
+        a.innerHTML = `Hozzáadás`;
 
-for (let i = 1; i <= menu.length; i++) {
-    const colDiv = document.createElement('div');
-    colDiv.classList.add('col-10', 'offset-1', 'offset-sm-0', 'col-md-6', 'col-lg-4', 'my-4');
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
-    const newImg = document.createElement('img');
-    newImg.classList.add('rounded-top');
-    newImg.src = `${baseURL}${i}.jpg`;
-    const cardBodyDiv = document.createElement('div');
-    cardBodyDiv.classList.add('card-body');
-    const h5 = document.createElement('h5');
-    h5.classList.add('card-title', 'my-3');
-    h5.innerHTML = `${menu[i].name}`;
-    const p = document.createElement('p');
-    p.classList.add('card-text', 'p-height', 'descFont');
-    p.innerHTML = `${menu[i].desc}`;
-    const inputDiv = document.createElement('div');
-    inputDiv.classList.add('form-outline', 'd-flex', 'justify-content-center', 'my-1', 'mb-3');
-    const input = document.createElement('input');
-    input.classList.add('form-control', 'text-center');
-    input.setAttribute('id', 'typeNumber');
-    input.setAttribute('type', 'number');
-    input.setAttribute('style', 'width: 5rem');
-    input.setAttribute('min', 1);
-    input.setAttribute('max', 5);
-    const pPrice = document.createElement('p');
-    pPrice.classList.add('text-center', 'text-primary', 'fw-bold', 'priceText');
-    pPrice.innerHTML = `${menu[i].price}`;
-    const a = document.createElement('a');
-    a.classList.add('btn', 'btn-outline-warning', 'kosarBtn');
-    a.innerHTML = `Hozzáadás`;
-
-    cardDiv.appendChild(newImg);
-    cardDiv.appendChild(cardBodyDiv);
-    cardBodyDiv.appendChild(h5);
-    cardBodyDiv.appendChild(p);
-    cardBodyDiv.appendChild(inputDiv);
-    inputDiv.appendChild(input);
-    cardBodyDiv.appendChild(pPrice);
-    cardBodyDiv.appendChild(a);
-    colDiv.appendChild(cardDiv);
-    container.appendChild(colDiv);
+        cardDiv.appendChild(newImg);
+        cardDiv.appendChild(cardBodyDiv);
+        cardBodyDiv.appendChild(h5);
+        cardBodyDiv.appendChild(p);
+        cardBodyDiv.appendChild(inputDiv);
+        inputDiv.appendChild(input);
+        cardBodyDiv.appendChild(pPrice);
+        cardBodyDiv.appendChild(a);
+        colDiv.appendChild(cardDiv);
+        destination.appendChild(colDiv);
+    }
 }
+insertItems(1, 10, container);
+
+// INSERT GYROS
+insertItems(11, 14, container2);
+// for (let i = 11; i < 15; i++) {
+//     const colDiv = document.createElement('div');
+//     colDiv.classList.add('col-10', 'offset-1', 'offset-md-0', 'col-md-6', 'col-lg-4', 'my-4');
+//     const cardDiv = document.createElement('div');
+//     cardDiv.classList.add('card');
+//     const newImg = document.createElement('img');
+//     newImg.classList.add('rounded-top');
+//     newImg.src = `${baseURL}${i}.jpg`;
+//     const cardBodyDiv = document.createElement('div');
+//     cardBodyDiv.classList.add('card-body');
+//     const h5 = document.createElement('h5');
+//     h5.classList.add('card-title', 'my-3', 'text-uppercase');
+//     h5.innerHTML = `${menu[i].name}`;
+//     const p = document.createElement('p');
+//     p.classList.add('card-text', 'p-height', 'descFont');
+//     p.innerHTML = `${menu[i].desc}`;
+//     const inputDiv = document.createElement('div');
+//     inputDiv.classList.add('form-outline', 'd-flex', 'justify-content-center', 'my-1', 'mb-3');
+//     const input = document.createElement('input');
+//     input.classList.add('form-control', 'text-center');
+//     input.setAttribute('id', 'typeNumber');
+//     input.setAttribute('type', 'number');
+//     input.setAttribute('style', 'width: 5rem');
+//     input.setAttribute('min', 1);
+//     input.setAttribute('max', 5);
+//     const pPrice = document.createElement('p');
+//     pPrice.classList.add('text-center', 'text-primary', 'fw-bold', 'priceText');
+//     pPrice.innerHTML = `${menu[i].price}`;
+//     const a = document.createElement('a');
+//     a.classList.add('btn', 'btn-outline-warning', 'kosarBtn');
+//     a.innerHTML = `Hozzáadás`;
+
+//     cardDiv.appendChild(newImg);
+//     cardDiv.appendChild(cardBodyDiv);
+//     cardBodyDiv.appendChild(h5);
+//     cardBodyDiv.appendChild(p);
+//     cardBodyDiv.appendChild(inputDiv);
+//     inputDiv.appendChild(input);
+//     cardBodyDiv.appendChild(pPrice);
+//     cardBodyDiv.appendChild(a);
+//     colDiv.appendChild(cardDiv);
+//     container2.appendChild(colDiv);
+
+// }
