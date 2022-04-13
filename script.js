5c92a239ecc58dbc6f09baadf58257c2842b4a4b
Bence Pentek <pntkbence@gmail.com>
2022-04-13 17:22:45 +0200

    Adding items to cart

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const container3 = document.querySelector('#container3');
 const pizzaScroll = document.querySelector('.pizzaScroll');
 const baseURL = 'https://raw.githubusercontent.com/BncPntk/Pizza/main/images/menu/'
 
+// NUMBER OF ITEMS IN CART
+const badgeNmb = document.querySelector('#cartBtnBadge');
+
 
 const menu = [
     {},
@@ -118,6 +121,42 @@ const menu = [
         desc: ``,
         price: '450',
     },
+    {
+        id: 18,
+        name: '0,5l Fanta citrom-bodza',
+        desc: ``,
+        price: '450',
+    },
+    {
+        id: 19,
+        name: '0,5l FuzeTea Zero Citrom zöld tea',
+        desc: ``,
+        price: '450',
+    },
+    {
+        id: 20,
+        name: '0,5l FuzeTea Citrom fekete tea',
+        desc: ``,
+        price: '450',
+    },
+    {
+        id: 21,
+        name: '0,5l Fanta narancs',
+        desc: ``,
+        price: '450',
+    },
+    {
+        id: 22,
+        name: '0,5l Naturaqua szénsavmentes',
+        desc: ``,
+        price: '350',
+    },
+    {
+        id: 23,
+        name: '0,33l CocaCola Zero Citrom',
+        desc: ``,
+        price: '350',
+    },
 
 
 ]
@@ -125,29 +164,36 @@ const menu = [
 
 
 
-// FORMIN PRICE
-for (let i = 1; i < 15; i++) {
-    let temp = menu[i].price.slice(1);
-    let newPrice = `HUF ${menu[i].price[0]},${temp}`
-    // console.log(newPrice);
-    menu[i].price = newPrice;
+// FORMAT PRICE
+for (let i = 1; i < 24; i++) {
+    if (menu[i].price.length >= 4) {
+        let temp = menu[i].price.slice(1);
+        let newPrice = `HUF ${menu[i].price[0]},${temp}`
+        // console.log(newPrice);
+        menu[i].price = newPrice;
+    } else {
+        let newPrice = `HUF ${menu[i].price}`
+        menu[i].price = newPrice;
+    }
+
 }
 
-
 // INSERT PIZZA
-const insertItems = function (imageFrom, imageTo, destination, imgType) {
+const insertItems = function (imageFrom, imageTo, destination, titleMargin, imgType, imgSizeAtr, imgSizeClass) {
     for (let i = imageFrom; i <= imageTo; i++) {
         const colDiv = document.createElement('div');
         colDiv.classList.add('col-10', 'offset-1', 'offset-md-0', 'col-md-6', 'col-lg-4', 'my-4');
+        colDiv.setAttribute('id', menu[i].id);
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
         const newImg = document.createElement('img');
-        newImg.classList.add('rounded-top');
+        newImg.classList.add('rounded-top', imgSizeClass);
+        newImg.setAttribute('style', imgSizeAtr);
         newImg.src = `${baseURL}${i}.${imgType}`;
         const cardBodyDiv = document.createElement('div');
         cardBodyDiv.classList.add('card-body');
         const h5 = document.createElement('h5');
-        h5.classList.add('card-title', 'my-3', 'text-uppercase');
+        h5.classList.add('card-title', titleMargin, 'text-uppercase');
         h5.innerHTML = `${menu[i].name}`;
         const p = document.createElement('p');
         p.classList.add('card-text', 'p-height', 'descFont');
@@ -161,11 +207,12 @@ const insertItems = function (imageFrom, imageTo, destination, imgType) {
         input.setAttribute('style', 'width: 5rem');
         input.setAttribute('min', 1);
         input.setAttribute('max', 5);
+        input.setAttribute('required', '');
         const pPrice = document.createElement('p');
         pPrice.classList.add('text-center', 'text-primary', 'fw-bold', 'priceText');
         pPrice.innerHTML = `${menu[i].price}`;
         const a = document.createElement('a');
-        a.classList.add('btn', 'btn-outline-warning', 'kosarBtn');
+        a.classList.add('btn', 'btn-outline-warning', 'kosarBtn', 'cartButton');
         a.innerHTML = `Hozzáadás`;
 
         cardDiv.appendChild(newImg);
@@ -180,54 +227,82 @@ const insertItems = function (imageFrom, imageTo, destination, imgType) {
         destination.appendChild(colDiv);
     }
 }
-insertItems(1, 10, container, 'jpg');
+
+insertItems(1, 10, container, 'my-3', 'jpg');
 
 // INSERT GYROS
-insertItems(11, 14, container2, 'jpg');
+insertItems(11, 14, container2, 'my-3', 'jpg');
 
 // INSERT DRINKS
-insertItems(15, 23, container3, 'png');
-// for (let i = 11; i < 15; i++) {
-//     const colDiv = document.createElement('div');
-//     colDiv.classList.add('col-10', 'offset-1', 'offset-md-0', 'col-md-6', 'col-lg-4', 'my-4');
-//     const cardDiv = document.createElement('div');
-//     cardDiv.classList.add('card');
-//     const newImg = document.createElement('img');
-//     newImg.classList.add('rounded-top');
-//     newImg.src = `${baseURL}${i}.jpg`;
-//     const cardBodyDiv = document.createElement('div');
-//     cardBodyDiv.classList.add('card-body');
-//     const h5 = document.createElement('h5');
-//     h5.classList.add('card-title', 'my-3', 'text-uppercase');
-//     h5.innerHTML = `${menu[i].name}`;
-//     const p = document.createElement('p');
-//     p.classList.add('card-text', 'p-height', 'descFont');
-//     p.innerHTML = `${menu[i].desc}`;
-//     const inputDiv = document.createElement('div');
-//     inputDiv.classList.add('form-outline', 'd-flex', 'justify-content-center', 'my-1', 'mb-3');
-//     const input = document.createElement('input');
-//     input.classList.add('form-control', 'text-center');
-//     input.setAttribute('id', 'typeNumber');
-//     input.setAttribute('type', 'number');
-//     input.setAttribute('style', 'width: 5rem');
-//     input.setAttribute('min', 1);
-//     input.setAttribute('max', 5);
-//     const pPrice = document.createElement('p');
-//     pPrice.classList.add('text-center', 'text-primary', 'fw-bold', 'priceText');
-//     pPrice.innerHTML = `${menu[i].price}`;
-//     const a = document.createElement('a');
-//     a.classList.add('btn', 'btn-outline-warning', 'kosarBtn');
-//     a.innerHTML = `Hozzáadás`;
+insertItems(15, 23, container3, 'my-0', 'png', 'max-width:50%', 'mx-auto');
 
-//     cardDiv.appendChild(newImg);
-//     cardDiv.appendChild(cardBodyDiv);
-//     cardBodyDiv.appendChild(h5);
-//     cardBodyDiv.appendChild(p);
-//     cardBodyDiv.appendChild(inputDiv);
-//     inputDiv.appendChild(input);
-//     cardBodyDiv.appendChild(pPrice);
-//     cardBodyDiv.appendChild(a);
-//     colDiv.appendChild(cardDiv);
-//     container2.appendChild(colDiv);
 
-// }
+// GO TO TOP BTN
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("btnTop").style.display = "block";
+    } else {
+        document.getElementById("btnTop").style.display = "none";
+    }
+}
+
+function btnUp() {
+    window.scrollTo(0, 0);
+}
+
+// ADD TO CARD BTN
+let itemInCartNmb = 0;
+let itemSet = new Set();
+let buttons = document.querySelectorAll('.cartButton');
+let quantityInputs = document.querySelectorAll('#typeNumber');
+let quantityValue = 0;
+
+// FINAL VALUES
+let finalName = '';
+let finalQty = 0;
+let finalPrice = 0;
+let grandTotal = 0;
+
+
+let inCart = [{
+    name: '',
+    qty: '',
+    price: '',
+}];
+let inputs = ['',];
+
+quantityInputs.forEach((btn, nmb) => {
+    inputs.push(btn);
+
+})
+
+buttons.forEach((btn, nmb) => {
+    btn.addEventListener('click', e => {
+
+        if (!itemSet.has(nmb + 1)) {
+            itemSet.add(nmb + 1);
+            itemInCartNmb++;
+
+            if (inputs[nmb + 1].value == 0) {
+            } else {
+                quantityValue = inputs[nmb + 1].value
+            }
+        }
+
+        // FORMATTING DATA TO USE WITH CART
+        badgeNmb.innerHTML = itemInCartNmb
+        finalQty = quantityValue;
+        finalName = menu[nmb + 1].name;
+        finalPrice = menu[nmb + 1].price;
+        finalPrice = finalPrice.replace('HUF ', '');
+        finalPrice = finalPrice.replace(',', '');
+
+        grandTotal = Number(finalPrice) * finalQty;
+        console.log('Név: ', finalName);
+        console.log('Végösszeg: ', grandTotal);
+        console.log('Egységár: ', finalPrice);
+        console.log('db:', quantityValue);
+    });
+
+});
